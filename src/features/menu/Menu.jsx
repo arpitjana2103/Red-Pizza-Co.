8d264e499679ab93ceed152c46ce83c7bf3f0560
Arpit Jana <arpitjana2103@gmail.com>
2023-11-14 17:24:16 +0530

    Loading Menu data with React Router Loader Func

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,16 @@
+/* eslint-disable react-refresh/only-export-components */
+import {useLoaderData} from 'react-router-dom';
+import {getMenu} from '../../services/apiRestaurant';
+
 function Menu() {
-  return <h1>Menu</h1>;
+    const menu = useLoaderData();
+    console.log(menu);
+    return <h1>Menu</h1>;
+}
+
+export async function loader() {
+    const menu = await getMenu();
+    return menu;
 }
 
 export default Menu;

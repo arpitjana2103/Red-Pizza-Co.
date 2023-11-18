decc8cc795ac23cfe13e20b26cf3a9bc6a292f0d
Arpit Jana <arpitjana2103@gmail.com>
2023-11-18 11:49:21 +0530

    Integrated Tailwind CSS

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,14 +1,14 @@
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
-    return (
-        <div>
-            <Link to="/">Fast React Pizza Co.</Link>
-            <SearchOrder />
-            <p>Arpit</p>
-        </div>
-    );
+  return (
+    <div className="bg-yellow-500">
+      <Link to="/">Fast React Pizza Co.</Link>
+      <SearchOrder />
+      <p>Arpit</p>
+    </div>
+  );
 }
 
 export default Header;

206682420e3eda28417ae5b9af87bdf7f5c22821
Arpit Jana <arpitjana2103@gmail.com>
2023-11-18 09:56:33 +0530

    Creating New Order with React Router Action Feature

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,9 @@
-import {useState} from 'react';
-
+/* eslint-disable react-refresh/only-export-components */
 // https://uibakery.io/regex-library/phone-number
+
+import {Form, redirect} from 'react-router-dom';
+import {createOrder} from '../../services/apiRestaurant';
+
 const isValidPhone = (str) =>
     /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(
         str
@@ -38,7 +41,7 @@ function CreateOrder() {
         <div>
             <h2>Ready to order? Let&apos;s go!</h2>
 
-            <form>
+            <Form method="POST">
                 <div>
                     <label>First Name</label>
                     <input type="text" name="customer" required />
@@ -67,16 +70,36 @@ function CreateOrder() {
                         // onChange={(e) => setWithPriority(e.target.checked)}
                     />
                     <label htmlFor="priority">
-                        Want to yo give your order priority?
+                        Want to you give your order priority?
                     </label>
                 </div>
 
                 <div>
+                    <input
+                        type="hidden"
+                        name="cart"
+                        value={JSON.stringify(cart)}
+                    />
                     <button>Order now</button>
                 </div>
-            </form>
+            </Form>
         </div>
     );
+}
+
+export async function action({request}) {
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData);
+
+    const order = {
+        ...data,
+        cart: JSON.parse(data.cart),
+        priority: data.priority === 'on',
+    };
+
+    const newOrder = await createOrder(order);
+
+    return redirect(`/order/${newOrder.id}`);
 }
 
 export default CreateOrder;
